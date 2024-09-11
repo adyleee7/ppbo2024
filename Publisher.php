bb425c1fb5cd34c72f20b892f5f06c121cfdbddf
adyleee7 <dinaadillah20@gmail.com>
2024-09-11 14:06:37 +0700

    5.5.1. Praktikum 5A - Memisahkan Class

---
+++ Publisher.php
@@ -0,0 +1,19 @@
+<?php
+
+
+class Publisher
+{
+    public string $name;
+    public string $address;
+    private string $phone;
+
+    public function setPhone(string $phone): void
+    {
+        $this->phone = $phone;
+    }
+
+    public function getPhone(): string
+    {
+        return $this->phone;
+    }
+}
