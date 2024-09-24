08514a4dd4f8259d9b7d6a831eebbac13097902e
adyleee7 <dinaadillah20@gmail.com>
2024-09-24 19:03:08 +0700

    Praktikum 5E - Menggunakan Pewarisan

---
+++ App/Admin/TenagaKependidikan.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Admin;
+
+class TenagaKependidikan extends Pegawai {
+    public int $gaji_pokok;
+
+    public function cuti(): void {
+        echo "{$this->nama} sedang cuti";
+    }
+}
